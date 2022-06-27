59412acbba2754d1cc212246ac96ce924f0d99b9
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-06-27 22:13:54 +0200

    :sparkles: Add Knight, Rook, Pawn classes

---
+++ Core.Tests/Pieces/RookTests.cs
@@ -0,0 +1,20 @@
+﻿using Core.Pieces;
+using Core.Shared;
+using Xunit;
+
+namespace Core.Tests.Pieces;
+
+public class RookTests
+{
+    [Theory]
+    [InlineData(Color.White, 'R')]
+    [InlineData(Color.Black, 'r')]
+    public void Constructor_SetsNameBasedOnColor(Color color, char expected)
+    {
+        var rook = new Rook(color);
+        
+        var actual = rook.Name;
+        
+        Assert.Equal(expected, actual);
+    }
+}
