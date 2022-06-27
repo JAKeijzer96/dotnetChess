87aa3a1fc0dec6cadd04acebc6d52da7f85418fa
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-06-27 22:06:49 +0200

    :sparkles: Add King and Queen classes

---
+++ Core/MoveValidators/KingMoveValidator.cs
@@ -0,0 +1,11 @@
+﻿using Core.ChessBoard;
+
+namespace Core.MoveValidators;
+
+public class KingMoveValidator : IMoveValidator
+{
+    public bool IsValidMove(Board board, Square @from, Square to)
+    {
+        throw new NotImplementedException();
+    }
+}
