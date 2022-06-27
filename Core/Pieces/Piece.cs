70c8ae5aeef421550141a78b5dc7d2a29396b04b
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-06-27 18:20:37 +0200

    :sparkles: Add Bishop

---
+++ Core/Pieces/Piece.cs
@@ -0,0 +1,29 @@
+﻿using Core.ChessBoard;
+using Core.MoveValidators;
+using Core.Shared;
+
+namespace Core.Pieces;
+
+public abstract class Piece
+{
+    private readonly IMoveValidator _moveValidator;
+    private readonly Color _color;
+    public char Name { get; set; }
+    
+    protected Piece(Color color, IMoveValidator moveValidator)
+    {
+        _color = color;
+        _moveValidator = moveValidator;
+    }
+
+    public bool HasSameColor(Piece piece)
+    {
+        return _color.Equals(piece._color);
+    }
+
+    public bool IsValidMove(Board board, Square from, Square to)
+    {
+        return _moveValidator.IsValidMove(board, from, to);
+    }
+
+}
---
+++ Core/Shared/Color.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Shared;
+
+public enum Color
+{
+    White,
+    Black
+}
