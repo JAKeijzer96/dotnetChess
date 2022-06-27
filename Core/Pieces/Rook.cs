7d8783b6e313a888f20b3fc4e1c3a71f02efd84c
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-06-27 22:13:54 +0200

    :sparkles: Add Knight, Rook, Pawn classes

---
+++ Core/Pieces/Rook.cs
@@ -0,0 +1,12 @@
+﻿using Core.MoveValidators;
+using Core.Shared;
+
+namespace Core.Pieces;
+
+public class Rook : Piece
+{
+    public Rook(Color color) : base(color, new RookMoveValidator())
+    {
+        Name = color == Color.White ? 'R' : 'r';
+    }
+}
