8163eda5c71e8159f395658c86cb4bd6ea6728d3
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-09-23 22:51:55 +0200

    Remove obsolete private methods in Board

--- Core/ChessBoard/Board.cs
+++ Core/ChessBoard/Board.cs
@@ -121,62 +121,6 @@ public class Board
 
     private void SetupBoardWithStartingPosition()
     {
-        // SetupBoardFromBoardFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
-        SetupWhitePiecesStartingPosition();
-        SetupWhitePawnsStartingPosition();
-        SetupEmptySquaresStartingPosition();
-        SetupBlackPawnsStartingPosition();
-        SetupBlackPiecesStartingPosition();
-    }
-
-    private void SetupWhitePiecesStartingPosition()
-    {
-        _squares[0, 0] = new Square(0, 0, new Rook(Color.White));
-        _squares[1, 0] = new Square(1, 0, new Knight(Color.White));
-        _squares[2, 0] = new Square(2, 0, new Bishop(Color.White));
-        _squares[3, 0] = new Square(3, 0, new Queen(Color.White));
-        _squares[4, 0] = new Square(4, 0, new King(Color.White));
-        _squares[5, 0] = new Square(5, 0, new Bishop(Color.White));
-        _squares[6, 0] = new Square(6, 0, new Knight(Color.White));
-        _squares[7, 0] = new Square(7, 0, new Rook(Color.White));
-    }
-
-    private void SetupWhitePawnsStartingPosition()
-    {
-        for (int file = 0; file < BoardSize; file++)
-        {
-            _squares[file, 1] = new Square(file, 1, new Pawn(Color.White));
-        }
-    }
-
-    private void SetupEmptySquaresStartingPosition()
-    {
-        for (var file = 0; file < BoardSize; file++)
-        {
-            for (var rank = 2; rank < 6; rank++)
-            {
-                _squares[file, rank] = new Square(file, rank);
-            }
-        }
-    }
-
-    private void SetupBlackPawnsStartingPosition()
-    {
-        for (int file = 0; file < BoardSize; file++)
-        {
-            _squares[file, 6] = new Square(file, 6, new Pawn(Color.Black));
-        }
-    }
-
-    private void SetupBlackPiecesStartingPosition()
-    {
-        _squares[0, 7] = new Square(0, 7, new Rook(Color.Black));
-        _squares[1, 7] = new Square(1, 7, new Knight(Color.Black));
-        _squares[2, 7] = new Square(2, 7, new Bishop(Color.Black));
-        _squares[3, 7] = new Square(3, 7, new Queen(Color.Black));
-        _squares[4, 7] = new Square(4, 7, new King(Color.Black));
-        _squares[5, 7] = new Square(5, 7, new Bishop(Color.Black));
-        _squares[6, 7] = new Square(6, 7, new Knight(Color.Black));
-        _squares[7, 7] = new Square(7, 7, new Rook(Color.Black));
+        SetupBoardFromBoardFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
     }
 }
