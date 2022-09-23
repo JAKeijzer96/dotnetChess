c1d5909d5ba44e69e790744756d3f159a030990a
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-09-23 13:54:18 +0200

    Setup FenParser structure

---
+++ Core/ChessGame/FenGameState.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.ChessGame;
+
+public class FenGameState
+{
+    private object Board;
+    private object Turn;
+    private object Castling;
+    private object EnPassant;
+    private object HalfMoveCount;
+    private object FullMoveCount;
+
+    public FenGameState(object board, object turn, object castling, object enPassant, object halfMoveCount, object fullMoveCount)
+    {
+        Board = board;
+        Turn = turn;
+        Castling = castling;
+        EnPassant = enPassant;
+        HalfMoveCount = halfMoveCount;
+        FullMoveCount = fullMoveCount;
+    }
+}
---
+++ Core/Parsers/FenParser.cs
@@ -0,0 +1,71 @@
+﻿using Core.ChessBoard;
+using Core.ChessGame;
+using Core.Exceptions;
+
+namespace Core.Parsers;
+
+public class FenParser
+{
+    string fenTestic = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+    public FenGameState Parse(string fen)
+    {
+        string[] splitFen = SplitFen(fen);
+
+        var board = ParseBoard(splitFen[0]);
+        var turn = ParseTurn(splitFen[1]);
+        var castling = ParseCastling(splitFen[2]);
+        var enPassant = ParseEnPassant(splitFen[3]);
+        var halfMoveCount = ParseHalfMoveCount(splitFen[4]);
+        var fullMoveCount = ParseFullMoveCount(splitFen[5]);
+
+        return new FenGameState(board, turn, castling, enPassant, halfMoveCount, fullMoveCount);
+    }
+
+
+    private string[] SplitFen(string fen)
+    {
+        if (fen == null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        string[] fenParts = fen.Split(' ');
+        if (fenParts.Length != 6)
+        {
+            throw new InvalidFenException("FEN string must have 6 parts");
+        }
+
+        return fenParts;
+    }
+
+    private Board ParseBoard(string boardPart)
+    {
+        return BoardFactory.ParsePartialFenNotation(boardPart);
+    }
+
+    private object ParseTurn(string s)
+    {
+        throw new NotImplementedException();
+    }
+
+    private object ParseCastling(string s)
+    {
+        throw new NotImplementedException();
+    }
+
+    private object ParseEnPassant(string s)
+    {
+        throw new NotImplementedException();
+    }
+
+    private object ParseHalfMoveCount(string s)
+    {
+        throw new NotImplementedException();
+    }
+
+    private object ParseFullMoveCount(string s)
+    {
+        throw new NotImplementedException();
+    }
+}
