b30fedea6cc86b665274431f50e5cdc0867be210
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-09-24 23:20:54 +0200

    Add BishopMoveValidator

---
+++ Core.Test/MoveValidators/BishopMoveValidatorTest.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Core.ChessBoard;
+using Core.MoveValidators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Test.MoveValidators;
+
+[TestClass]
+public class BishopMoveValidatorTest
+{
+    [TestMethod]
+    public void IsValidMove_WhenBoardIsNull_ThrowsArgumentNullException()
+    {
+        Board? board = null;
+        var validator = new BishopMoveValidator();
+        var fromSquare = new Square(0, 0);
+        var toSquare = new Square(1, 1);
+
+        void Act()
+        {
+            validator.IsValidMove(board!, fromSquare, toSquare);
+        }
+
+        var exception = Assert.ThrowsException<ArgumentNullException>((Action) Act);
+        Assert.AreEqual("Value cannot be null. (Parameter 'board')", exception.Message);
+    }
+    
+    [TestMethod]
+    public void IsValidMove_WhenStartingSquareIsNull_ReturnsFalse()
+    {
+        var board = new Board();
+        var validator = new BishopMoveValidator();
+        Square? fromSquare = null;
+        var toSquare = new Square(1, 1);
+
+        void Act()
+        {
+            validator.IsValidMove(board, fromSquare!, toSquare);
+        }
+
+        var exception = Assert.ThrowsException<ArgumentNullException>((Action) Act);
+        Assert.AreEqual("Value cannot be null. (Parameter 'from')", exception.Message);
+    }
+    
+    [TestMethod]
+    public void IsValidMove_WhenDestinationSquareIsNull_ReturnsFalse()
+    {
+        var board = new Board();
+        var validator = new BishopMoveValidator();
+        var fromSquare = new Square(0, 0);
+        Square? toSquare = null;
+
+        void Act()
+        {
+            validator.IsValidMove(board, fromSquare, toSquare!);
+        }
+
+        var exception = Assert.ThrowsException<ArgumentNullException>((Action) Act);
+        Assert.AreEqual("Value cannot be null. (Parameter 'to')", exception.Message);
+    }
+    
+    [TestMethod]
+    public void IsValidMove_WhenPieceIsNull_ReturnsFalse()
+    {
+        var board = new Board("8/8/8/8/4B3/8/4P3/k3K3");
+        var validator = new BishopMoveValidator();
+        var fromSquare = board.GetSquare("d5");
+        var toSquare = board.GetSquare("c6");
+        
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+        
+        Assert.AreEqual(false, result);
+    }
+    
+    [TestMethod]
+    public void IsValidMove_ToSameSquare_ReturnsFalse()
+    {
+        var board = new Board("8/8/8/8/4B3/8/4P3/k3K3");
+        var validator = new BishopMoveValidator();
+        var fromSquare = board.GetSquare("e4");
+        var toSquare = board.GetSquare("e4");
+        
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+        
+        Assert.AreEqual(false, result);
+    }
+    
+    [DataTestMethod]
+    [DataRow("e4", "b1")]
+    [DataRow("e4", "a8")]
+    [DataRow("e4", "h7")]
+    [DataRow("e4", "h1")]
+    public void IsValidMove_ForDiagonalMove_ReturnsTrue(string from, string to)
+    {
+        var board = new Board("8/8/8/8/4B3/8/4P3/k3K3");
+        var validator = new BishopMoveValidator();
+        var fromSquare = board.GetSquare(from);
+        var toSquare = board.GetSquare(to);
+        
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+        
+        Assert.IsTrue(result);
+    }
+
+    [DataTestMethod]
+    [DataRow("e4", "e8")]
+    [DataRow("e4", "e1")]
+    [DataRow("e4", "a4")]
+    [DataRow("e4", "h4")]
+    public void IsValidMove_ForMoveThatIsNotDiagonal_ReturnsFalse(string from, string to)
+    {
+        var board = new Board("8/8/8/8/4B3/8/4P3/k3K3");
+        var validator = new BishopMoveValidator();
+        var fromSquare = board.GetSquare(from);
+        var toSquare = board.GetSquare(to);
+        
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+        
+        Assert.AreEqual(false, result);
+    }
+
+    [TestMethod]
+    public void IsValidMove_WhenMoveIsBlockedByPieceOfOppositeColor_ReturnsFalse()
+    {
+        var board = new Board("1N6/4P3/3b4/8/8/r5k1/8/5K2");
+        var validator = new BishopMoveValidator();
+        var fromSquare = board.GetSquare("d6");
+        var toSquare = board.GetSquare("f8");
+        
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+        
+        Assert.AreEqual(false, result);
+    }
+
+    [TestMethod]
+    public void IsValidMove_WhenMoveIsBlockedByPieceOfSameColor_ReturnsFalse()
+    {
+        var board = new Board("1N6/4P3/3b4/8/8/r5k1/8/5K2");
+        var validator = new BishopMoveValidator();
+        var fromSquare = board.GetSquare("d6");
+        var toSquare = board.GetSquare("h2");
+        
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+        
+        Assert.AreEqual(false, result);
+    }
+    
+    [TestMethod]
+    public void IsValidMove_WhenDestinationSquareHasPieceOfOppositeColor_ReturnsTrue()
+    {
+        var board = new Board("1N6/4P3/3b4/8/8/r5k1/8/5K2");
+        var validator = new BishopMoveValidator();
+        var fromSquare = board.GetSquare("d6");
+        var toSquare = board.GetSquare("b8");
+        
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+        
+        Assert.AreEqual(true, result);
+    }
+    
+    [TestMethod]
+    public void IsValidMove_WhenDestinationSquareHasPieceOfSameColor_ReturnsFalse()
+    {
+        var board = new Board("1N6/4P3/3b4/8/8/r5k1/8/5K2");
+        var validator = new BishopMoveValidator();
+        var fromSquare = board.GetSquare("d6");
+        var toSquare = board.GetSquare("a3");
+        
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+        
+        Assert.AreEqual(false, result);
+    }
+
+}
--- Core/MoveValidators/BishopMoveValidator.cs
+++ Core/MoveValidators/BishopMoveValidator.cs
@@ -6,23 +6,31 @@ public class BishopMoveValidator : IMoveValidator
 {
     public bool IsValidMove(Board board, Square from, Square to)
     {
-        if (from == to)
-            return false;
+        if (!IsValidDestinationSquare(board, from, to)) return false;
 
-        if (Math.Abs(from.File - to.File) != Math.Abs(from.Rank - to.Rank))
-            return false;
+        int xDirection = from.File < to.File ? 1 : -1;
+        int yDirection = from.Rank < to.Rank ? 1 : -1;
 
-        var xDirection = from.File < to.File ? 1 : -1;
-        var yDirection = from.Rank < to.Rank ? 1 : -1;
-
-        for (int x = from.File + xDirection, y = from.Rank + yDirection; x != to.File; x += xDirection, y += yDirection)
+        for (int file = from.File + xDirection, rank = from.Rank + yDirection;
+             file != to.File;
+             file += xDirection, rank += yDirection)
         {
-            if (board.GetSquare(x, y).Piece != null)
+            if (board.GetSquare(file, rank).IsOccupied())
                 return false;
         }
 
         return true;
     }
-    
-    
+
+    private static bool IsValidDestinationSquare(Board? board, Square? from, Square? to)
+    {
+        if (board is null) throw new ArgumentNullException(nameof(board));
+        if (from is null) throw new ArgumentNullException(nameof(from));
+        if (to is null) throw new ArgumentNullException(nameof(to));
+
+        return from.Piece is not null &&
+               from != to &&
+               Math.Abs(from.File - to.File) == Math.Abs(from.Rank - to.Rank) &&
+               to.Piece?.Color != from.Piece.Color;
+    }
 }
