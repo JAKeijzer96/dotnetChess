8649c91987a26d213abd36c68aa5c3d0b8aff192
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-09-24 15:12:15 +0200

    Add == operator for Piece

--- Core.Test/Pieces/PieceTest.cs
+++ Core.Test/Pieces/PieceTest.cs
@@ -16,4 +16,57 @@ public class PieceTest
         
         Assert.AreEqual("k", result);
     }
+    
+    [TestMethod]
+    public void GetHashCode_OfEqualPieces_AreTheSame()
+    {
+        var hashcode1 = new King(Color.Black).GetHashCode();
+        var hashcode2 = new King(Color.Black).GetHashCode();
+        
+        Assert.AreEqual(hashcode1, hashcode2);
+    }
+    
+    [TestMethod]
+    public void GetHashCode_OfUnequalPieces_AreDifferent()
+    {
+        var hashcode1 = new King(Color.White).GetHashCode();
+        var hashcode2 = new King(Color.Black).GetHashCode();
+        
+        Assert.AreNotEqual(hashcode1, hashcode2);
+    }
+    
+    [TestMethod]
+    public void EqualsOperator_ForEqualPieces_ReturnsTrue()
+    {
+        Piece left = new King(Color.Black);
+        Piece right = new King(Color.Black);
+        
+        var result = left == right;
+        
+        Assert.AreEqual(true, result);
+    }
+    
+    [TestMethod]
+    public void EqualsOperator_ForUnequalPiecesOfSameColor_ReturnsFalse()
+    {
+        Piece left = new King(Color.Black);
+        Piece right = new Bishop(Color.Black);
+        
+        var result = left == right;
+        
+        Assert.AreEqual(false, result);
+    }
+    
+    [TestMethod]
+    public void EqualsOperator_ForSamePieceTypeOfDifferentColor_ReturnsFalse()
+    {
+        Piece left = new Rook(Color.White);
+        Piece right = new Rook(Color.Black);
+        
+        var result = left == right;
+        
+        Assert.AreEqual(false, result);
+    }
+    
+    
 }
