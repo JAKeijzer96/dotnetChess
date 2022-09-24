9f2c5943c73591bb9c44d49960c7e23b9afe012c
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-09-24 15:12:15 +0200

    Add == operator for Piece

--- Core/Pieces/Piece.cs
+++ Core/Pieces/Piece.cs
@@ -8,7 +8,7 @@ public abstract class Piece
     private readonly IMoveValidator _moveValidator;
     public Color Color { get; }
     public char Name { get; protected init; }
-    
+
     protected Piece(Color color, IMoveValidator moveValidator)
     {
         Color = color;
@@ -18,5 +18,33 @@ public abstract class Piece
     public override string ToString()
     {
         return Name.ToString();
+    }
+
+    public static bool operator ==(Piece left, Piece right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Piece x, Piece y)
+    {
+        return !(x == y);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Piece) obj);
+    }
+
+    protected bool Equals(Piece other)
+    {
+        return Name == other.Name;
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
     }
 }
