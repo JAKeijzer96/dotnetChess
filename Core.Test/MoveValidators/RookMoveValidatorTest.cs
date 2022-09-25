ad477490a646f392cf6af446f1e2c01be75d9742
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-09-25 10:28:54 +0200

    Implement RookMoveValidator

---
+++ Core.Test/MoveValidators/RookMoveValidatorTest.cs
@@ -0,0 +1,82 @@
+﻿using Core.ChessBoard;
+using Core.MoveValidators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Test.MoveValidators;
+
+[TestClass]
+public class RookMoveValidatorTest
+{
+    [DataTestMethod]
+    [DataRow("c5", "a5")]
+    [DataRow("c5", "f5")]
+    public void IsValidMove_ForHorizontalMove_ReturnsTrue(string from, string to)
+    {
+        var board = new Board("8/2b5/8/2R5/8/8/k1K5/8");
+        var validator = new RookMoveValidator();
+        var fromSquare = board.GetSquare(from);
+        var toSquare = board.GetSquare(to);
+
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+
+        Assert.AreEqual(true, result);
+    }
+
+    [DataTestMethod]
+    [DataRow("c5", "c6")]
+    [DataRow("c5", "c3")]
+    public void IsValidMove_ForVerticalMove_ReturnsTrue(string from, string to)
+    {
+        var board = new Board("8/2b5/8/2R5/8/8/k1K5/8");
+        var validator = new RookMoveValidator();
+        var fromSquare = board.GetSquare(from);
+        var toSquare = board.GetSquare(to);
+
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+
+        Assert.AreEqual(true, result);
+    }
+    
+    [DataTestMethod]
+    [DataRow("c5", "a7")]
+    [DataRow("c5", "b7")]
+    [DataRow("c5", "g1")]
+    [DataRow("c5", "a2")]
+    public void IsValidMove_ForMoveThatIsNotHorizontalOrVertical_ReturnsFalse(string from, string to)
+    {
+        var board = new Board("8/2b5/8/2R5/8/8/k1K5/8");
+        var validator = new RookMoveValidator();
+        var fromSquare = board.GetSquare(from);
+        var toSquare = board.GetSquare(to);
+
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+
+        Assert.AreEqual(false, result);
+    }
+
+    [TestMethod]
+    public void IsValidMove_WhenMoveIsBlockedByPieceOfOppositeColor_ReturnsFalse()
+    {
+        var board = new Board("8/2b5/8/2R5/8/8/k1K5/8");
+        var validator = new RookMoveValidator();
+        var fromSquare = board.GetSquare("c5");
+        var toSquare = board.GetSquare("c8");
+
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+
+        Assert.AreEqual(false, result);
+    }
+
+    [TestMethod]
+    public void IsValidMove_WhenMoveIsBlockedByPieceOfSameColor_ReturnsFalse()
+    {
+        var board = new Board("8/2b5/8/2R5/8/8/k1K5/8");
+        var validator = new RookMoveValidator();
+        var fromSquare = board.GetSquare("c5");
+        var toSquare = board.GetSquare("c1");
+
+        var result = validator.IsValidMove(board, fromSquare, toSquare);
+
+        Assert.AreEqual(false, result);
+    }
+}
--- Core/MoveValidators/RookMoveValidator.cs
+++ Core/MoveValidators/RookMoveValidator.cs
@@ -4,8 +4,37 @@ namespace Core.MoveValidators;
 
 public class RookMoveValidator : MoveValidator
 {
-    public override bool IsValidMove(Board board, Square @from, Square to)
+    public override bool IsValidMove(Board board, Square from, Square to)
     {
-        throw new NotImplementedException();
+        if (!IsValidDestinationSquare(board, from, to)) return false;
+        
+        if (from.File == to.File)
+        {
+            var yDirection = from.Rank < to.Rank ? 1 : -1;
+            for (var rank = from.Rank + yDirection; rank != to.Rank; rank += yDirection)
+            {
+                if (board.GetSquare(from.File, rank).IsOccupied()) return false;
+            }
+
+            return true;
+        }
+        if (from.Rank == to.Rank)
+        {
+            var xDirection = from.File < to.File ? 1 : -1;
+            for (var file = from.File + xDirection; file != to.File; file += xDirection)
+            {
+                if (board.GetSquare(file, from.Rank).IsOccupied()) return false;
+            }
+
+            return true;
+        }
+        
+        return false;
+    }
+
+    protected override bool IsValidDestinationSquare(Board? board, Square? from, Square? to)
+    {
+        return base.IsValidDestinationSquare(board, from, to) &&
+               (from!.File == to!.File || from.Rank == to.Rank);
     }
 }
