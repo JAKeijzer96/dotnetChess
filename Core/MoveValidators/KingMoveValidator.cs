e7b82d1b04515e9c02c04c6808ed966fdd579c95
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-09-25 09:53:46 +0200

    Finish KingMoveValidatorTest

--- Core/MoveValidators/KingMoveValidator.cs
+++ Core/MoveValidators/KingMoveValidator.cs
@@ -8,11 +8,11 @@ public class KingMoveValidator : MoveValidator
     {
         return IsValidDestinationSquare(board, from, to);
     }
-    
+
     protected override bool IsValidDestinationSquare(Board? board, Square? from, Square? to)
     {
         return base.IsValidDestinationSquare(board, from, to) &&
-               Math.Abs(from!.File - to!.File) <= 1 && 
+               Math.Abs(from!.File - to!.File) <= 1 &&
                Math.Abs(from.Rank - to.Rank) <= 1;
     }
 }
