2f4776a01f3b03fcb45d02cbdaf05bf8e9c0326d
JAKeijzer96 <jasperkeijzer96@gmail.com>
2022-09-25 11:02:10 +0200

    Implement KnightMoveValidator

--- Core/MoveValidators/KnightMoveValidator.cs
+++ Core/MoveValidators/KnightMoveValidator.cs
@@ -4,8 +4,14 @@ namespace Core.MoveValidators;
 
 public class KnightMoveValidator : MoveValidator
 {
-    public override bool IsValidMove(Board board, Square @from, Square to)
+    public override bool IsValidMove(Board board, Square from, Square to)
     {
-        throw new NotImplementedException();
+        return IsValidDestinationSquare(board, from, to);
+    }
+
+    protected override bool IsValidDestinationSquare(Board? board, Square? from, Square? to)
+    {
+        return base.IsValidDestinationSquare(board, from, to) &&
+               Math.Abs(from!.File - to!.File) * Math.Abs(from.Rank - to.Rank) == 2;
     }
 }
