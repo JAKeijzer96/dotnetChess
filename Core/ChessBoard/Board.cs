e025913c68feb0808500d13380e80196b4775c49
Jasper Keijzer <s1131685@student.hsleiden.nl>
2023-06-29 19:58:13 +0200

    Minor Type-fix

--- Core/ChessBoard/Board.cs
+++ Core/ChessBoard/Board.cs
@@ -10,7 +10,7 @@ public class Board
     public const int BoardSize = 8;
     private const string DefaultStartingPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
 
-    public Square this[String squareName] => GetSquare(squareName);
+    public Square this[string squareName] => GetSquare(squareName);
 
     public Board() : this(DefaultStartingPosition)
     {
